8d71b3436c4f2602f1e597b118dab1b4ea5d44e2
leonbyrdjr <leonbyrdjr@Leons-MBP.hsd1.tx.comcast.net>
2022-09-05 17:07:28 -0500

    commit tree eb6e35c5a2cc5cec111e328fbe01dad8ea74ea8c

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const palindrome = word.toLowerCase()
+  const palindrome2 = word.toLowerCase().split('').reverse().join('')
+
+  if(palindrome === palindrome2){
+    return true
+  }
+  else{
+    return false
+  }
 }
 
 /* 
